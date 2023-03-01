90ef2561369e1329870a62827ac267db6d423214
pransiskaloy <bagas.franciscarlo.t@gmail.com>
2023-03-01 17:52:18 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/olsis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.olsis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Container(
+          child: GetStarted(),
+        ),
+      ),
+    );
+  }
+}
+
+class GetStarted extends StatefulWidget {
+  const GetStarted({Key? key}) : super(key: key);
+
+  @override
+  State<GetStarted> createState() => _GetStartedState();
+}
+
+class _GetStartedState extends State<GetStarted> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('Get Started Page')),
+    );
+  }
+}
